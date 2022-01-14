dad6289ab08cf3ebe065908050798d3056e5fe42
Taylor Boyd <taylor.boyd@oit.edu>
2022-01-13 16:52:04 -0800

    Created the node class and default constructor.

--- CST211-Lab2-Test/List.h
+++ CST211-Lab2-Test/List.h
@@ -1,6 +1,15 @@
 #ifndef LIST_H
 #define LIST_H
 
+template<typename T>
+class Node
+{
+	T data;
+	Node* nextElement;
+	Node* prevElement;
+};
 
+template<typename T>
+Node<T>::Node() : nextElement(nullptr), prevElement(nullptr) {}
 
 #endif
