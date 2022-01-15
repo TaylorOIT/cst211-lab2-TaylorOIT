8d0fd2c22349536ea37799c7b3990a69326911bf
Taylor Boyd <taylor.boyd@oit.edu>
2022-01-14 17:02:59 -0800

    Created DoublyLinkList class and added a isEmpty() method.

--- CST211-Lab2-Test/CST211-Lab2-Test.cpp
+++ CST211-Lab2-Test/CST211-Lab2-Test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include "List.h"
 
 int main() {
-
 
 
 }
--- CST211-Lab2-Test/List.h
+++ CST211-Lab2-Test/List.h
@@ -1,15 +1,37 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream> 
+
 template<typename T>
 class Node
 {
+private:
 	T data;
 	Node* nextElement;
 	Node* prevElement;
+public:
+	Node(); // Default ctor
 };
 
 template<typename T>
 Node<T>::Node() : nextElement(nullptr), prevElement(nullptr) {}
+
+template<typename T>
+class DoublyLinkList {
+private:
+	Node* head;
+public:
+	bool isEmpty();
+};
+
+template<typename T>
+bool DoublyLinkList<T>::isEmpty()
+{
+	if (head == nullptr)
+		return true;
+	else
+		return false;
+}
 
 #endif
