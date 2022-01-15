44ca83d7d21b651bb2a96ba3fd04e4d96c084c3d
Taylor Boyd <taylor.boyd@oit.edu>
2022-01-15 11:20:53 -0800

    Added a copy constructor to my List class.

--- CST211-Lab2-Test/List.h
+++ CST211-Lab2-Test/List.h
@@ -17,7 +17,7 @@ public:
 };
 
 template<typename T>
-Node<T>::Node() : nextElement(nullptr), prevElement(nullptr) {}
+Node<T>::Node() : data(), nextElement(nullptr), prevElement(nullptr) {}
 
 template<typename T>
 class List {
@@ -26,9 +26,10 @@ private:
 	Node<T>* tail;
 public:
 	List(); // default constructor
+	List(const List<T>& copy); // copy constructor
 	~List(); // destructer
 	void Purge(); // deletes the entire linklist
-	void Extract(T data);
+	void Extract(T data); // deletes a selected element in the link list
 	bool isEmpty(); // checks if the link list is empty
 	void Prepend(T data); // add data at the front of the link list
 	void Append(T data); // add data at the end of the link list
@@ -38,11 +39,24 @@ public:
 	void PrintBackwards(); // print the entire link list backwards
 	const T& First(); // returns the first element in the link list
 	const T& Last(); // return the last element in the link list
+
+	Node<T>* getHead();
+	Node<T>* getTail();
 	
 };
 
 template<typename T>
-List<T>::List() : head(nullptr),tail(nullptr) {}
+List<T>::List() : head(nullptr) {}
+
+template<typename T>
+List<T>::List(const List<T>& copy)
+{
+	Node<T>* curr = copy.head;
+	while (curr != nullptr) {
+		Append(curr->data);
+		curr = curr->nextElement;
+	}
+}
 
 template<typename T>
 List<T>::~List()
@@ -50,16 +64,11 @@ List<T>::~List()
 	Node<T>* curr = head;
 	if (curr != nullptr)
 		curr = curr->nextElement;
-	cout << "---------------------------------------------------" << endl;
-	cout << "~List" << endl;
-	PrintForwards();
 	while (head != nullptr) {
 		curr = head;
 		head = head->nextElement;
 		delete curr;
-		PrintForwards();
 	}
-	cout << "---------------------------------------------------" << endl;
 }
 
 template<typename T>
@@ -76,22 +85,20 @@ void List<T>::Purge()
 template<typename T>
 void List<T>::Extract(T data)
 {
+	if (isEmpty() == true)
+		throw(Exception("The link list is empty!"));
+	
 	Node<T>* curr = head;
 	Node<T>* prev = nullptr;
 
 	bool deleted = false;
-
-	if (isEmpty()) {
-		cout << "List is Empty" << endl;
-		return; 
-	}
 
 	if (curr->data == data) {
 		head = head->nextElement;
 		delete curr;
 		deleted = true;
 	}
-	
+
 	while (curr != nullptr && deleted == false) {
 		if (data == curr->data) {
 			prev->nextElement = curr->nextElement;
@@ -104,12 +111,8 @@ void List<T>::Extract(T data)
 		curr = curr->nextElement;
 	}
 
-	if (deleted == false) {
-		cout << data << " does not exist!" << endl;
-	}
-	else {
-		cout << data << " was deleted!" << endl;
-	}
+	if (deleted == false)
+		throw(Exception("The value does not exist!"));
 }
 
 template<typename T>
@@ -131,29 +134,38 @@ void List<T>::Prepend(T data)
 		head->prevElement = newNode;
 	}
 	head = newNode;
-	cout << data << " inserted at front of list!" << endl;
 
 }
 
 template<typename T>
 void List<T>::Append(T data)
 {
-	Node<T>* newNode = new Node<T>;
-	newNode->data = data;
-	
-	Node<T>* curr = head;
-	while (curr->nextElement != nullptr) {
-		curr = curr->nextElement;
-		newNode->prevElement = curr;
-	}
+	if (isEmpty() != true) {
+		Node<T>* newNode = new Node<T>;
+		newNode->data = data;
 
-	curr->nextElement = newNode;
-	cout << data << " inserted at back of list!" << endl;
+		Node<T>* curr = head;
+		while (curr->nextElement != nullptr) {
+			curr = curr->nextElement;
+			newNode->prevElement = curr;
+		}
+
+		curr->nextElement = newNode;
+	}
+	else
+	{
+		Prepend(data);
+	}
 }
 
 template<typename T>
 void List<T>::InsertAfter(T new_item, T existing_item)
 {
+	bool Inserted = false;
+
+	if (isEmpty() == true)
+		throw(Exception("The link list is empty!"));
+	
 	Node<T>* newNode = new Node<T>;
 	newNode->data = new_item;
 
@@ -163,16 +175,25 @@ void List<T>::InsertAfter(T new_item, T existing_item)
 			newNode->prevElement = curr;
 			newNode->nextElement = curr->nextElement;
 			curr->nextElement = newNode;
+			Inserted = true;
 			break;
 		}
 		else
 			curr = curr->nextElement;
 	}
+
+	if (Inserted == false)
+		throw(Exception("The existing item does not exist!"));
 }
 
 template<typename T>
 void List<T>::InsertBefore(T new_item, T existing_item)
 {
+	bool Inserted = false;
+	
+	if (isEmpty() == true)
+		throw(Exception("The link list is empty!"));
+
 	Node<T>* newNode = new Node<T>;
 	newNode->data = new_item;
 
@@ -181,17 +202,19 @@ void List<T>::InsertBefore(T new_item, T existing_item)
 	Node<T>* temp = curr;
 	temp = temp->nextElement;
 
-	if (curr->data == existing_item) {
-		Append(new_item);
+	if (head->data == existing_item) {
+		Prepend(new_item);
 		return;
 	}
 	// if head is equal to the existing item, append at front of list.
+
 
 	while ((curr != nullptr) && (temp != nullptr)) {
 		if (temp->data == existing_item) {
 			newNode->prevElement = curr;
 			newNode->nextElement = curr->nextElement;
 			curr->nextElement = newNode;
+			Inserted = true;
 			break;
 		}
 		else {
@@ -199,6 +222,10 @@ void List<T>::InsertBefore(T new_item, T existing_item)
 			temp = temp->nextElement;
 		}
 	}
+
+	if (Inserted == false)
+		throw(Exception("The existing item does not exist!"));
+
 }
 
 template<typename T>
@@ -239,19 +266,37 @@ void List<T>::PrintBackwards()
 template<typename T>
 const T& List<T>::First()
 {
-	Node<T>* curr = head;
-	curr->nextElement;
-	return curr->data;
+	if (isEmpty() == true)
+	{
+		throw(Exception("The link list is empty!"));
+	}
+		Node<T>* curr = head;
+		curr->nextElement;
+		return curr->data;
 }
 
 template<typename T>
 const T& List<T>::Last()
 {
+	if (isEmpty() == true)
+		throw(Exception("The link list is empty!"));
 	Node<T>* curr = head;
 	while (curr->nextElement != nullptr) {
 		curr = curr->nextElement;
 	}
 	return curr->data;
+}
+
+template<typename T>
+Node<T>* List<T>::getHead()
+{
+	return head;
+}
+
+template<typename T>
+Node<T>* List<T>::getTail()
+{
+	return tail;
 }
 
 #endif
